a6ec067495b2cde338bf87f43f9782a0af70e3b0
Adrian <adrian_zinko@op.pl>
2020-08-25 21:38:57 +0200

    feat: connect to mongodb

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const postRouter= require('./routes/posts');
 const app = express();
+
+mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+console.log("Connected to Database");
+}).catch((err) => {
+    console.log("Not Connected to Database ERROR! ", err);
+});
 
 app.set('view engine', 'ejs');
 
